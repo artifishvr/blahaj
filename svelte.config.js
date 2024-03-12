426d06bbb7bf8952cb4704147da4c56e6f46f19d
artificial <59352535+artificialbutter@users.noreply.github.com>
2024-03-11 17:28:14 -0700

    tailwind

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,21 @@
-import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-static";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+  kit: {
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: "build",
+      assets: "build",
+      fallback: undefined,
+      precompress: false,
+      strict: true,
+    }),
+  },
+
+  preprocess: [vitePreprocess({})],
 };
 
 export default config;
