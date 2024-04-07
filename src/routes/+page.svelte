50e63f64b5df1c29eeded38aca78950fce7b739f
artifish <dev@arti.gay>
2024-04-07 16:16:34 -0700

    improve music handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,27 @@
 <script>
-    import '../app.pcss';
-    import * as THREE from 'three';
-    import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
-    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
-    import { onMount } from 'svelte';
+    import "../app.pcss";
+    import * as THREE from "three";
+    import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+    import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+    import { onMount } from "svelte";
 
     let bpm = 20;
 
     onMount(() => {
         // three
-        const container = document.getElementById('canvasFrame');
+        const container = document.getElementById("canvasFrame");
 
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize(window.innerWidth, window.innerHeight);
         container.appendChild(renderer.domElement);
 
         const scene = new THREE.Scene();
-        const camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(
+            70,
+            window.innerWidth / window.innerHeight,
+            0.1,
+            1000,
+        );
         scene.background = 0xffffff;
         camera.position.z = -0.8;
         camera.position.x = 0.45;
@@ -24,7 +29,7 @@
         const loader = new GLTFLoader();
 
         loader.load(
-            'blahaj.glb',
+            "blahaj.glb",
             function (gltf) {
                 scene.add(gltf.scene);
             },
@@ -53,40 +58,43 @@
         }
         animate();
 
-        const audio = document.createElement('audio');
-        audio.src = 'discordholdmusic.mp3';
-        audio.loop = true;
-        audio.volume = 0.1;
-        document.body.appendChild(audio);
-
-        // watching stuff
-        document.addEventListener('click', () => {
-            audio.play();
-            plausible('PlayMusic');
-        });
-        document.addEventListener('visibilitychange', () => {
-            if (document.hidden) {
-                audio.pause();
-            } else {
-                audio.play();
-            }
-        });
-        window.addEventListener('resize', () => {
+        window.addEventListener("resize", () => {
             camera.aspect = window.innerWidth / window.innerHeight;
             camera.updateProjectionMatrix();
             renderer.setSize(window.innerWidth, window.innerHeight);
         });
 
-        const analyticsScript = document.createElement('script');
+        const analyticsScript = document.createElement("script");
         analyticsScript.defer = true;
-        analyticsScript.dataset.domain = 'blahaj.arti.gay';
-        analyticsScript.src = 'https://a.arti.lol/js/script.tagged-events.js';
+        analyticsScript.dataset.domain = "blahaj.arti.gay";
+        analyticsScript.src = "https://a.arti.lol/js/script.tagged-events.js";
         document.head.appendChild(analyticsScript);
-        const plausibleExtra = document.createElement('script');
+        const plausibleExtra = document.createElement("script");
         plausibleExtra.innerHTML =
-            'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }';
+            "window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }";
         document.head.appendChild(plausibleExtra);
     });
+
+    let firstplay = true;
+    let pausemusic = true;
+    let volume = 0.1;
+    function playMusic() {
+        pausemusic = !pausemusic;
+        if (firstplay) {
+            plausible("PlayMusic");
+            firstplay = false;
+        }
+    }
 </script>
 
+<div class="fixed bottom-0 right-0 m-4">
+    <button
+        on:click={playMusic}
+        class="  bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 rounded-md py-2 px-4 text-white"
+        >{pausemusic ? "Play" : "Pause"} Music</button>
+</div>
+
 <div id="canvasFrame" class="-z-40"></div>
+
+<audio src="discordholdmusic.mp3" loop bind:paused={pausemusic} bind:volume
+></audio>
