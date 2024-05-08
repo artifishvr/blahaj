5fd7b9f3460ee16147d9f8ca32dbbaca5a46672a
Arti <59352535+artifishvr@users.noreply.github.com>
2024-05-07 23:25:25 -0700

    main concept done

--- src/components/MusicPlayerModal.svelte
+++ src/components/MusicPlayerModal.svelte
@@ -6,15 +6,31 @@
 
     let bpmDetermined = true;
 
+    let downloadurl = "";
+
     async function playMusic() {
+        // Set the source with the HTML Audio Node
+        const track = document.getElementById("track");
+
+        const response = await fetch("https://co.wuk.sh/api/json", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+            body: JSON.stringify({
+                url: downloadurl,
+                isAudioOnly: true,
+            }),
+        });
+        const data = await response.json();
+        console.log(data);
+        track.src = data.url;
         bpmDetermined = false;
         const audioContext = new AudioContext();
 
         const realtimeAnalyzerNode =
             await realtimeBpm.createRealTimeBpmProcessor(audioContext);
-
-        // Set the source with the HTML Audio Node
-        const track = document.getElementById("track");
         track.play();
         const source = audioContext.createMediaElementSource(track);
         const lowpass = realtimeBpm.getBiquadFilter(audioContext);
@@ -24,9 +40,6 @@
         source.connect(audioContext.destination);
 
         realtimeAnalyzerNode.port.onmessage = (event) => {
-            // if (event.data.message === "BPM") {
-            //     console.log("BPM", event.data.data.bpm);
-            // }
             if (event.data.message === "BPM_STABLE") {
                 console.log("BPM_STABLE", event.data.data.bpm[0].tempo);
                 bpm.set(event.data.data.bpm[0].tempo);
@@ -40,20 +53,20 @@
     class="{show ? '' : 'opacity-0 pointer-events-none'} fixed left-0 top-0 flex h-full w-full items-center justify-center transition duration-500 ease-out">
     <div
         class="relative m-12 flex max-h-full max-w-3xl flex-col items-center justify-center rounded-md border border-gray-600/50 bg-gray-400 bg-opacity-10 bg-clip-padding p-8 text-white backdrop-blur-md backdrop-filter">
-        <h1 class="text-center text-xl font-medium">
-            Custom Music Player (WIP)
+        <h1 class="text-center text-2xl font-medium pb-2">
+            Music Player
         </h1>
         
         <audio
             crossorigin="anonymous"
-            src="music.wav"
-            id="track"
-            volume="0.1"></audio>
+            id="track"></audio>
+
+        <input placeholder="Youtube, Soundcloud" type="text" bind:value={downloadurl} class="rounded-md border border-gray-600/50 bg-gray-400 bg-opacity-10 bg-clip-padding p-2 text-white backdrop-blur-md backdrop-filter text-center">
         <button
             on:click="{playMusic}"
-            class="rounded-md bg-gray-400 bg-opacity-20 bg-clip-padding px-4 py-2 text-white backdrop-blur-sm backdrop-filter">
-            Analyse BPM
+            class="rounded-md mt-2 bg-gray-400 bg-opacity-20 bg-clip-padding px-4 py-2 text-white backdrop-blur-sm backdrop-filter">
+            Play
         </button>
-        <p class="{bpmDetermined ? 'hidden' : "animate-pulse"}">blahaj is listening! give them a moment c:</p>
+        <p class="pt-2 {bpmDetermined ? 'hidden' : "animate-pulse"}">blahaj is listening! give them a moment c:</p>
     </div>
 </div>
