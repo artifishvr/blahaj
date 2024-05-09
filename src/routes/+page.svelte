9c030c282f80d89ac229513139dc5cb57d411a73
Arti <59352535+artifishvr@users.noreply.github.com>
2024-05-08 18:01:19 -0700

    fullscreen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,7 @@
     import LoadingModal from '../components/LoadingModal.svelte';
     import MusicPlayerModal from '../components/MusicPlayerModal.svelte';
     import { bpm } from '$lib/stores';
+    import screenfull from 'screenfull';
 
     let bpm_value;
 
@@ -123,6 +124,14 @@
             firstplay = false;
         }
     }
+
+    let isFullscreen = false;
+
+    if (screenfull.isEnabled) {
+        screenfull.on('change', () => {
+            isFullscreen = screenfull.isFullscreen;
+        });
+    }
 </script>
 
 <svelte:head>
@@ -132,6 +141,61 @@
 <LoadingModal {loading} {loadpercent} />
 
 <MusicPlayerModal bind:show={showCustomMusicPlayer} />
+
+<div class="fixed top-0 right-0 m-4 text-gray-400">
+    <button
+        on:click={() => {
+            if (screenfull.isEnabled) {
+                screenfull.request();
+                isFullscreen = true;
+            } else {
+                alert('fullscreen not supported :c');
+            }
+        }}
+        class="{isFullscreen
+            ? 'hidden'
+            : ''} rounded-md bottom-0 bg-gray-400 bg-opacity-20 bg-clip-padding p-2 text-white backdrop-blur-sm backdrop-filter">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-arrows-maximize"
+            ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M16 4l4 0l0 4" /><path d="M14 10l6 -6" /><path
+                d="M8 20l-4 0l0 -4" /><path d="M4 20l6 -6" /><path d="M16 20l4 0l0 -4" /><path d="M14 14l6 6" /><path d="M8 4l-4 0l0 4" /><path
+                d="M4 4l6 6" /></svg>
+    </button>
+
+    <button
+        on:click={() => {
+            if (screenfull.isEnabled) {
+                screenfull.exit();
+                isFullscreen = false;
+            }
+        }}
+        class="{isFullscreen
+            ? ''
+            : 'hidden'} rounded-md bottom-0 bg-gray-400 bg-opacity-20 bg-clip-padding p-2 text-white backdrop-blur-sm backdrop-filter">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="icon icon-tabler icons-tabler-outline icon-tabler-arrows-minimize"
+            ><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M5 9l4 0l0 -4" /><path d="M3 3l6 6" /><path d="M5 15l4 0l0 4" /><path
+                d="M3 21l6 -6" /><path d="M19 9l-4 0l0 -4" /><path d="M15 9l6 -6" /><path d="M19 15l-4 0l0 4" /><path d="M15 15l6 6" /></svg>
+    </button>
+</div>
 
 <div class="fixed bottom-0 right-0 m-4 flex space-x-2">
     <button
