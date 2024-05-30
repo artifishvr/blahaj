cb9b087c8d034d5fe4c41de413cde80041b8933f
Arti <59352535+artifishvr@users.noreply.github.com>
2024-05-29 23:26:45 -0700

    loading quotes

--- src/components/LoadingModal.svelte
+++ src/components/LoadingModal.svelte
@@ -1,24 +1,26 @@
 <script>
     export let loading;
     export let loadpercent;
+    import { quotes } from '$lib/quotes';
+    import { browser } from '$app/environment';
+
+    let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 </script>
 
 <div
-    class="{loading ? '' : 'opacity-0 pointer-events-none'} pointer-events-none fixed left-0 top-0 flex h-full w-full items-center justify-center transition duration-500 ease-out">
+    class="{loading && browser
+        ? ''
+        : 'opacity-0 pointer-events-none'} pointer-events-none fixed left-0 top-0 flex h-full w-full items-center justify-center transition duration-500 ease-out">
     <div
         class="relative m-12 flex max-h-full max-w-3xl flex-col items-center justify-center rounded-md border border-gray-600/50 bg-gray-400 bg-opacity-10 bg-clip-padding p-8 text-white backdrop-blur-md backdrop-filter">
-        <div
-            class="mb-4 h-12 w-12 animate-spin rounded-full border-4 border-t-4 border-gray-200 border-t-blue-500 ease-linear"></div>
-        <h1 class="text-center text-xl font-medium">Loading 3D...</h1>
-        <p class="text-center font-medium">
-            This may take a while on slower connections/devices.
-        </p>
+        <div class="mb-4 h-24 w-24 animate-spin ease-linear">
+            <img src="/favicon.png" alt="logo" class="w-full h-full" />
+        </div>
+        <p class="text-center text-md font-semibold pt-2">Did you know</p>
+        <p class="text-center text-sm font-medium">{randomQuote}</p>
 
-        <div
-            class="mt-4 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
-            <div
-                class="h-2.5 rounded-full bg-blue-500"
-                style="width: {loadpercent}%"></div>
+        <div class="mt-4 h-2.5 w-full rounded-full bg-gray-200 dark:bg-gray-700">
+            <div class="h-2.5 rounded-full bg-blue-500" style="width: {loadpercent}%"></div>
         </div>
     </div>
 </div>
